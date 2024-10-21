cfa661c807f036d80d259418588f6c160c25efa6
felipeFleite <felipefracalossileite@gmail.com>
2024-10-21 17:38:17 -0400

    sa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import Lista from "../Lista"
 export default function Home () {
     const [lista, setLista] = useState ([])
 
@@ -15,22 +15,6 @@ useEffect(() => {
 }
 receberListaProdutos()
 }, [])
-
-return(
-    <>
-    <h1>Lista de Produtos</h1>
-    <ul>
-        {lista.map(produto => 
-            <li key ={produto.id}>
-            <h2>{produto.title}</h2>
-            <p>{produto.description}</p>
-            <p>Preço: R${produto.price}</p>
-            <img src={produto.image} alt ={produto.title} width={100}/>
-            </li>
-)}
-    </ul>
-    </>
-)
 }
 
     
